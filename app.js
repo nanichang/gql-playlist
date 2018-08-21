dcdb3d0fa04d1f1dd1683d0613d51dabf2c78a2f
nanichang <nanipaul68@gmail.com>
2018-08-21 22:09:23 +0100

    added connection fix

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-/**
- * @author Nanichang Katzing
- * @email nanipaul68@gmail.com
- * @create date: 21 August, 2018
- * @modify date: 21 August, 2018
- * @modify by 
- */
-
-'use strict';
-// if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
-  require('dotenv').config();
-// };
-
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
@@ -21,17 +8,7 @@ const cors = require('cors');
 
 app.use(cors());
 
-const 
-  port = process.env.PORT,
-  host = process.env.HOST,
-  env = process.env.NODE_ENV,
-  user = process.env.DB_USER,
-  password = process.env.DB_PASSWORD,
-  db_host = process.env.DB_HOST,
-  db_port = process.env.DB_PORT,
-  database = process.env.DB_NAME;
-
-mongoose.connect(`mongodb://${user}:${password}@${db_host}:${db_port}/${database}`);
+mongoose.connect('mongodb://nani:secret1@ds125352.mlab.com:25352/graphql-apollo');
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 });
@@ -43,6 +20,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(port, () => {
-  console.log(`🚀 Server listening for request on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server listening for request on port 3000');
 });
